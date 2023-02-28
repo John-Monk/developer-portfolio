93411b4d23003384c0f3b2ea17797cb5056ce43f
John Monk <johntmonk1@gmail.com>
2023-02-27 22:36:53 -0600

    added header

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header>
+        <a href="https://johnmonk.dev/">johnmonk.dev</a>
+        
+      </header>
     </>
   )
 }
