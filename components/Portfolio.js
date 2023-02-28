f51e51b7418043e2fcae7455371f63733b9f9fc9
John Monk <johntmonk1@gmail.com>
2023-02-27 22:47:11 -0600

    created and wrote most markup for Portfolio section

---
+++ components/Portfolio.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Portfolio() {
+  return (
+    <section className="portfolio">
+        <section className="skills">
+            <h2 className="title">Skills</h2>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+        <section className="projects">
+            <h2 className="title">Projects</h2>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+    </section>
+  )
+}
