408469c188cc445bc4dd6d3cdf87126f36e0ef31
John Monk <johntmonk1@gmail.com>
2023-02-27 22:47:45 -0600

    import portfolio section, added to markup

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Nav from '@/components/Nav'
 import Hero from '@/components/Hero'
+import Portfolio from '@/components/Portfolio'
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Nav />
       </header>
       <Hero />
+      <Portfolio />
     </>
   )
 }
