84eac7dfb2fc26c073dc9755dbbb7927e1ca6f52
John Monk <johntmonk1@gmail.com>
2023-02-27 22:44:08 -0600

    added markup for Hero section

--- components/Hero.js
+++ components/Hero.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <section className="hero">
+        <p className="blurb">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit ducimus laudantium assumenda voluptatem? Magni error iure reiciendis sed exercitationem nulla, fugiat possimus porro ex sint!</p>
+            <ul className="social__media">
+                <li><a className='icon' href="#"></a></li>
+                <li><a className='icon' href="#"></a></li>
+                <li><a className='icon' href="#"></a></li>
+            </ul>
+            <div className="headshot">
+                <Image />
+            </div>
+    </section>
   )
 }
