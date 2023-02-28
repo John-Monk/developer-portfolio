ac0bbf0b19d92552ed8ac8a9dd1794f01b52c888
John Monk <johntmonk1@gmail.com>
2023-02-27 23:23:46 -0600

    added styling to hero section

--- components/Hero.js
+++ components/Hero.js
@@ -6,17 +6,17 @@ import {AiFillLinkedin, AiFillGithub, AiOutlineTwitter, AiOutlineMail} from 'rea
 
 export default function Hero() {
   return (
-    <section className="hero">
-        <div className="container">
-            <h1>Hi, I'm John</h1>
-            <p className="blurb">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit ducimus laudantium assumenda voluptatem? Magni error iure reiciendis sed exercitationem nulla, fugiat possimus porro ex sint!</p>
-                <ul className="social__media">
-                    <li><a className='icon' href="#"><AiFillLinkedin /></a></li>
-                    <li><a className='icon' href="#"><AiFillGithub /></a></li>
-                    <li><a className='icon' href="#"><AiOutlineTwitter /></a></li>
+    <section className={styles.hero}>
+        <div className={`container ${styles.content}`}>
+            <h1 className={styles.title}>Hi, I'm John</h1>
+            <p className={styles.blurb}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit ducimus laudantium assumenda voluptatem? Magni error iure reiciendis sed exercitationem nulla, fugiat possimus porro ex sint!</p>
+                <ul className={styles.social__media}>
+                    <li><a className={styles.icon} href="#"><AiFillLinkedin size='30px' /></a></li>
+                    <li><a className={styles.icon} href="#"><AiFillGithub size='30px' /></a></li>
+                    <li><a className={styles.icon} href="#"><AiOutlineTwitter size='30px' /></a></li>
                 </ul>
-                <div className="headshot">
-                    <Image src={headshot}/>
+                <div className={styles.headshot__container}>
+                    <Image className={styles.headshot} src={headshot}/>
                 </div>
         </div>
     </section>
