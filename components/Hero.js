41dd2a8ffb83dcdc861b9e8e687d649557f6e296
John Monk <johntmonk1@gmail.com>
2023-02-27 23:11:27 -0600

    added icons and headshot to markup

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from './Hero.module.css';
-import photo from '../public/headshot.jpg';
+import headshot from '../public/headshot.jpg';
 import {AiFillLinkedin, AiFillGithub, AiOutlineTwitter, AiOutlineMail} from 'react-icons/ai';
 
 export default function Hero() {
@@ -11,12 +11,12 @@ export default function Hero() {
             <h1>Hi, I'm John</h1>
             <p className="blurb">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit ducimus laudantium assumenda voluptatem? Magni error iure reiciendis sed exercitationem nulla, fugiat possimus porro ex sint!</p>
                 <ul className="social__media">
-                    <li><a className='icon' href="#"></a></li>
-                    <li><a className='icon' href="#"></a></li>
-                    <li><a className='icon' href="#"></a></li>
+                    <li><a className='icon' href="#"><AiFillLinkedin /></a></li>
+                    <li><a className='icon' href="#"><AiFillGithub /></a></li>
+                    <li><a className='icon' href="#"><AiOutlineTwitter /></a></li>
                 </ul>
                 <div className="headshot">
-                    <Image />
+                    <Image src={headshot}/>
                 </div>
         </div>
     </section>
