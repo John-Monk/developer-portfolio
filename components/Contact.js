42083775f527db247b89464b90454726be8680b6
John Monk <johntmonk1@gmail.com>
2023-02-27 22:53:03 -0600

    added basic markup for contact section

--- components/Contact.js
+++ components/Contact.js
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <section className="contact">
+        <h2 className="title">Get in touch</h2>
+        <div className="contact__container">
+            <form action="">
+                <ul>
+                    <li>
+                        <label htmlFor="name">Name</label>
+                        <input id='name' type="text" />
+                    </li>
+                    <li>
+                        <label htmlFor="email">E-Mail</label>
+                        <input id='email' type="text" />
+                    </li>
+                    <li>
+                        <label htmlFor="message">Message</label>
+                        <input id='message' type="text" />
+                    </li>
+                </ul>
+                <button>Submit</button>
+            </form>
+            <a href="#">johntmonk1@gmail.com</a>
+        </div>
+    </section>
   )
 }
