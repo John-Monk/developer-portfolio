0ed33f769b5e2d1ee8c6869b0ed73594f0c24cc3
John Monk <johntmonk1@gmail.com>
2023-03-08 12:44:12 -0600

    adjusted markup and adding initial styling

--- components/Contact.js
+++ components/Contact.js
@@ -3,29 +3,27 @@ import styles from './Contact.module.css';
 
 export default function Contact() {
   return (
-    <section className={styles.contact}>
-        <div className="container">
-            <h2 className="title">Get in touch</h2>
-            <div className={styles.contact__container}>
+    <section className={`container ${styles.contact}`}>
+        <h2 className={styles.title}>Get in touch</h2>
+        <div className={styles.contact__container}>
                 <form className={styles.form} action="">
                     <ul className={styles.list}>
                         <li className={styles.list__item}>
                             <label className={styles.label} htmlFor="name">Name</label>
                             <input className={styles.input} id='name' type="text" />
                         </li>
-                        <li>
+                        <li className={styles.list__item}>
                             <label className={styles.label} htmlFor="email">E-Mail</label>
                             <input className={styles.input} id='email' type="text" />
                         </li>
-                        <li>
+                        <li className={styles.list__item}>
                             <label className={styles.label} htmlFor="message">Message</label>
-                            <input className={styles.input} id='message' type="text" />
+                            <textarea className={`${styles.input} ${styles.message__input}`} id='message' type='text' />
                         </li>
                     </ul>
                     <button className={styles.button}>Submit</button>
                 </form>
-                <a className={styles.email} href="#">johntmonk1@gmail.com</a>
-            </div>
+            <a className={styles.email} href="#">johntmonk1@gmail.com</a>
         </div>
     </section>
   )
