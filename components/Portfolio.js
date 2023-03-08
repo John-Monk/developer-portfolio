92329d61753ce56ada076e541e7407d5a100f64d
John Monk <johntmonk1@gmail.com>
2023-03-08 00:49:31 -0600

    added initial styling to portfolio section

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -10,31 +10,34 @@ import nextjs from '../public/nextjs.svg';
 import nodejs from '../public/nodejs.svg';
 import sass from '../public/sass.svg';
 import reactjs from '../public/reactjs.svg';
+import Image from 'next/image';
 
 export default function Portfolio() {
   return (
     <section className="portfolio">
-        <div className="container">
+        <div className={`container ${styles.section__container}`}>
             <section className="skills">
-                <h2 className="title">Skills</h2>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
+                <h2 className={styles.title}>Skills</h2>
+                    <ul className={styles.skill__list}>
+                        <li className={styles.skill__item}><Image className={styles.skill} src={html} alt='HTML logo'/></li>
+                        <li className={styles.skill__item}><Image className={styles.skill} src={css} alt='CSS logo'/></li>
+                        <li className={styles.skill__item}><Image className={styles.skill} src={javascript} alt='JavaScript logo' /></li>
+                        <li className={styles.skill__item}><Image className={styles.skill} src={git} alt='GIT logo'/></li>
+                        <li className={styles.skill__item}><Image className={styles.skill} src={sass} alt='SASS logo'/></li>
+                        <li className={styles.skill__item}><Image className={styles.skill} src={reactjs} alt='Reactjs logo'/></li>
+                        <li className={styles.skill__item}><Image className={styles.skill} src={nextjs} alt='Nextjs logo'/></li>
+                        <li className={styles.skill__item}><Image className={styles.skill} src={express} alt='Express logo'/></li>
+                        <li className={styles.skill__item}><Image className={styles.skill} src={nodejs} alt='Nodejs logo'/></li>
+                        <li className={styles.skill__item}><Image className={styles.skill} src={mongodb} alt='MongoDB logo'/></li>
+                    </ul>
             </section>
             <section className="projects">
-                <h2 className="title">Projects</h2>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
+                <h2 className={styles.title}>Projects</h2>
+                    <ul className={styles.project__list}>
+                        <li className={styles.project__item}><Image /></li>
+                        <li className={styles.project__item}><Image /></li>
+                        <li className={styles.project__item}><Image /></li>
+                    </ul>
             </section>
         </div>
     </section>
