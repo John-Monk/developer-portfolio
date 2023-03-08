5f2b9424be176f1b5b3c2298ff5fa5fd89cdfa89
John Monk <johntmonk1@gmail.com>
2023-03-08 00:54:12 -0600

    added classnames that correlate to css module

--- components/Contact.js
+++ components/Contact.js
@@ -3,28 +3,28 @@ import styles from './Contact.module.css';
 
 export default function Contact() {
   return (
-    <section className="contact">
+    <section className={styles.contact}>
         <div className="container">
             <h2 className="title">Get in touch</h2>
-            <div className="contact__container">
-                <form action="">
-                    <ul>
-                        <li>
-                            <label htmlFor="name">Name</label>
-                            <input id='name' type="text" />
+            <div className={styles.contact__container}>
+                <form className={styles.form} action="">
+                    <ul className={styles.list}>
+                        <li className={styles.list__item}>
+                            <label className={styles.label} htmlFor="name">Name</label>
+                            <input className={styles.input} id='name' type="text" />
                         </li>
                         <li>
-                            <label htmlFor="email">E-Mail</label>
-                            <input id='email' type="text" />
+                            <label className={styles.label} htmlFor="email">E-Mail</label>
+                            <input className={styles.input} id='email' type="text" />
                         </li>
                         <li>
-                            <label htmlFor="message">Message</label>
-                            <input id='message' type="text" />
+                            <label className={styles.label} htmlFor="message">Message</label>
+                            <input className={styles.input} id='message' type="text" />
                         </li>
                     </ul>
-                    <button>Submit</button>
+                    <button className={styles.button}>Submit</button>
                 </form>
-                <a href="#">johntmonk1@gmail.com</a>
+                <a className={styles.email} href="#">johntmonk1@gmail.com</a>
             </div>
         </div>
     </section>
