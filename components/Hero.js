73865406d2e1b78bbecc3853fedaa3b5def9ae21
John Monk <johntmonk1@gmail.com>
2023-03-08 13:23:47 -0600

    moved sizing for social media icons to css rules to control size at different viewports

--- components/Hero.js
+++ components/Hero.js
@@ -6,19 +6,21 @@ import {AiFillLinkedin, AiFillGithub, AiOutlineTwitter, AiOutlineMail} from 'rea
 
 export default function Hero() {
   return (
-    <section className={styles.hero}>
-        <div className={`container ${styles.content}`}>
+    <section className={`container ${styles.hero}`}>
+          <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm John</h1>
-            <p className={styles.blurb}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit ducimus laudantium assumenda voluptatem? Magni error iure reiciendis sed exercitationem nulla, fugiat possimus porro ex sint!</p>
+            <p className={styles.blurb}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Odit ducimus laudantium assumenda voluptatem?</p>
                 <ul className={styles.social__media}>
-                    <li><a className={styles.icon} href="#"><AiFillLinkedin size='30px' /></a></li>
-                    <li><a className={styles.icon} href="#"><AiFillGithub size='30px' /></a></li>
-                    <li><a className={styles.icon} href="#"><AiOutlineTwitter size='30px' /></a></li>
+                    <li><a className={styles.icon} href="#"><AiFillLinkedin /></a></li>
+                    <li><a className={styles.icon} href="#"><AiFillGithub /></a></li>
+                    <li><a className={styles.icon} href="#"><AiOutlineTwitter /></a></li>
                 </ul>
-                <div className={styles.headshot__container}>
-                    <Image className={styles.headshot} src={headshot}/>
-                </div>
-        </div>
+            </div>
+            <div className={styles.headshot__container}>
+                <Image className={styles.headshot} src={headshot}/>
+            </div>
     </section>
   )
 }
