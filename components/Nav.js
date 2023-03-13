74e83a6e83aa922dbc0462722183ab5613ba2e34
John Monk <johntmonk1@gmail.com>
2023-03-12 21:46:29 -0500

    updated transitions and styling for nav active and hover states

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import styles from './Nav.module.css';
 
-export default function Nav() {
+export default function Nav({currentSection}) {
   return (
     <nav className={styles.nav}>
           <ul className={styles.item__list}>
-              <li className={styles.list__item}><a className={styles.item} href="#">Home</a></li>
-              <li className={`${styles.list__item} ${styles.active}`}><a className={styles.item} href="#">Portfolio</a></li>
-              <li className={styles.list__item}><a className={styles.item} href="#">Contact</a></li>
+              <li className={`${styles.list__item} ${currentSection === 'hero' ? styles.active : ''}`}><a className={styles.item} href="#">Home</a></li>
+              <li className={`${styles.list__item} ${currentSection === 'portfolio' ? styles.active : ''}`}><a className={styles.item} href="#portfolio">Portfolio</a></li>
+              <li className={`${styles.list__item} ${currentSection === 'contact' ? styles.active : ''}`}><a className={styles.item} href="#contact">Contact</a></li>
           </ul>
     </nav>
   )
