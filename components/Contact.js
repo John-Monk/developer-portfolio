12d1b2bc234faec41b45977fe4b0ac6434953672
John Monk <johntmonk1@gmail.com>
2023-03-15 00:21:39 -0500

    added appropraite attribute to submit form to netlify, also added submission function and appropraite attributes to inputs to clear form inputs on submit

--- components/Contact.js
+++ components/Contact.js
@@ -1,25 +1,39 @@
 import React from 'react'
 import styles from './Contact.module.css';
 import {AiOutlineMail} from 'react-icons/ai';
+import { useState } from 'react';
 
 export default function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const formSubmit = (e) => {
+        e.preventDefault();
+
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
+
+
   return (
     <section className={`container ${styles.contact}`}>
         <h2 className={styles.title}>Get in touch</h2>
         <div className={styles.contact__container}>
-                <form onSubmit={e => e.preventDefault()} className={styles.form} action="">
+                <form onSubmit={e => formSubmit(e)} className={styles.form} name="contact" action="POST" data-netlify="true">
                     <ul className={styles.list}>
                         <li className={styles.list__item}>
                             <label className={styles.label} htmlFor="name">Name</label>
-                            <input className={styles.input} id='name' type="text" />
+                            <input onChange={(e) => setName(e.target.value)} className={styles.input} id='name' type="text" value={name}/>
                         </li>
                         <li className={styles.list__item}>
                             <label className={styles.label} htmlFor="email">E-Mail</label>
-                            <input className={styles.input} id='email' type="text" />
+                            <input onChange={(e) => setEmail(e.target.value)} className={styles.input} id='email' type="text" value={email}/>
                         </li>
                         <li className={styles.list__item}>
                             <label className={styles.label} htmlFor="message">Message</label>
-                            <textarea className={`${styles.input} ${styles.message__input}`} rows='3' id='message' type='text' />
+                            <textarea onChange={(e) => setMessage(e.target.value)} className={`${styles.input} ${styles.message__input}`} rows='3' id='message' type='text' value={message}/>
                         </li>
                     </ul>
                     <button className={styles.button}>Submit</button>
