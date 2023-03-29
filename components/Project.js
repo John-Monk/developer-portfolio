63d58bd4b931ffb54f92563c2a70c6c1dae5b5e2
John Monk <johntmonk1@gmail.com>
2023-03-29 16:28:15 -0500

    created necessary markup for project component, passed in props for different project information and links

--- components/Project.js
+++ components/Project.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import styles from './Project.module.css';
+import Image from 'next/image';
 
-export default function Project() {
+export default function Project({title, description, live, code, image}) {
   return (
-    <div>Project</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.desc}>{description}</p>
+      <div className={styles.link__container}>
+        <a className={styles.link} href={live} target='_blank'>Live</a>
+        <a className={styles.link} href={code} target='_blank'>Code</a>
+      </div>
+      <Image className={styles.image} src={image} />
+    </div>
   )
 }
